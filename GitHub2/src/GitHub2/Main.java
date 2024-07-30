d34f90c6a11610f9e4b3e7ce5ec4b2cbb427d707
LENOVO <LENOVO@LAPTOP-961HLDPR>
2024-07-30 08:14:09 +0530

    New GitHUb

---
+++ GitHub2/src/GitHub2/Main.java
@@ -0,0 +1,11 @@
+package GitHub2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello GitHub");
+		System.out.println("Hyy git");
+		System.out.println("Hyy git");
+	}
+}
